9ab938e2acbd856bf4ff8cadd8202f9a7e1b1895
aryaman.sriram <aryaman.sriram@gmail.com>
2021-02-18 15:41:56 +0530

    Added moving func

--- scripts/wbt_separator.py
+++ scripts/wbt_separator.py
@@ -1,15 +1,28 @@
 import os
 import pandas as pd
-
+import shutil
 
 def read_predcsv(pred_path):
     return pd.read_csv(pred_path)
 
 
+def move_file(filenames,root_dir,new_dir,sub_folder_name):
+    save_dir = new_dir+sub_folder_name
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    else:
+        for file in filenames:
+            start_path = root_dir+file
+            end_path = save_dir+file
+            shutil.move(start_path,end_path)
 
 if __name__=="__main__":
     pred_path = "Data/transfer_01_predictions.csv"
     df = read_predcsv(pred_path)
-    ones = df[df["[Birds]Vs[EverythingElse]_Prediction"]==1]
-    zs = df[df["[Birds]Vs[EverythingElse]_Prediction"]==0]
+    ones = df[df["[Birds]Vs[EverythingElse]_Prediction"]==1]["wav_file"]
+    zs = df[df["[Birds]Vs[EverythingElse]_Prediction"]==0]["wav_file"]
+    move_file(ones,"Data/Transfer_01/","Segregated_Sounds/","1")
+    move_file(zs, "Data/Transfer_01/", "Segregated_Sounds/", "0")
+
+
 
