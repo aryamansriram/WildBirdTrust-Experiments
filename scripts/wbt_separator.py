a595180d28c1a2e5a69e6315394dcf0da4c7eef7
aryaman.sriram <aryaman.sriram@gmail.com>
2021-02-18 17:18:34 +0530

    Added cmd support

--- scripts/wbt_separator.py
+++ scripts/wbt_separator.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import shutil
+import argparse
 def read_predcsv(pred_path):
     return pd.read_csv(pred_path)
 
@@ -16,12 +17,22 @@ def move_file(filenames,root_dir,new_dir,sub_folder_name):
         shutil.move(start_path,end_path)
 
 if __name__=="__main__":
-    pred_path = "Data/transfer_01_predictions.csv"
+    DESCRIPTION = "This file separates positive and negative examples into their respective directories"
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument("-p","--path_of_predictions",action="store",help="path to the predictions csv")
+    parser.add_argument("-ps","--path_to_split_wavfiles_directory",action="store",help="path to wavfiles split")
+    parser.add_argument("-d","--dest",action="store",help="new folder where segregated files are to be stored")
+    result = parser.parse_args()
+    #pred_path = "Data/transfer_01_predictions.csv"
+    pred_path = result.path_of_predictions
+    source = result.path_to_split_wavfiles_directory
+    dest = result.dest
     df = read_predcsv(pred_path)
     ones = df[df["[Birds]Vs[EverythingElse]_Prediction"]==1]["wav_file"]
     zs = df[df["[Birds]Vs[EverythingElse]_Prediction"]==0]["wav_file"]
-    move_file(ones,"Data/Transfer_01_split/","Segregated_Sounds/","1/")
-    move_file(zs, "Data/Transfer_01_split/", "Segregated_Sounds/", "0/")
-
+    #move_file(ones,"Data/Transfer_01_split/","Segregated_Sounds/","1/")
+    #move_file(zs, "Data/Transfer_01_split/", "Segregated_Sounds/", "0/")
+    move_file(ones,source,dest,"1/")
+    move_file(zs,source,dest,"0/")
 
 
